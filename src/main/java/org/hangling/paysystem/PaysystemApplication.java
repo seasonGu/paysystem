aa3f2192a9b8fde8daed73b0c6865f3aaafc63e9
seaonGu <seasoncoding@qq.com>
2026-07-16 16:02:32 +0800

    init project

---
+++ src/main/java/org/hangling/paysystem/PaysystemApplication.java
@@ -0,0 +1,13 @@
+package org.hangling.paysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaysystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hangling/paysystem/PaysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hangling.paysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
